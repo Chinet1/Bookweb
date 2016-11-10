84f4b133f6d44e1d92689b91724c80a86625cd15
Chinet <chinetcom@gmail.com>
2016-11-10 19:10:55 +0100

    Created index, css and js files

---
+++ app/main.js
@@ -0,0 +1,16 @@
+﻿var formApp = angular.module('formApp',[]);
+
+formApp.controller('FormCtrl', ['$scope', function($scope) {
+	$scope.title = 'BookWeb';
+
+	$scope.books = [
+		{name:'Harry Potter i Kamień Filozoficzny'},
+		{name:'Design dla Hackerów'}
+	];
+
+	$scope.addBook = function () {
+		$scope.books.push({name:$scope.bookName});
+		$scope.bookName = '';
+	}
+
+}]);
